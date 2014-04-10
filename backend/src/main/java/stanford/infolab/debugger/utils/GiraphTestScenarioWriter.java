6b0c70e8c03016cf3ebda13b2c7c3cbc8e1a8832
Semih Salihoglu <semihsalihoglu@gmail.com>
2014-04-09 20:12:27 -0700

    - Reorganizing the directory structure. server, instrumentor, and editor are now all under src/main/java/stanford/infolab/debugger/ subdirectories of a top directory named backend. - Adding a pom.xml for the backend directory. (We're starting to use maven.) - Adding a protocol buffer for scenarios. For now contains a GiraphScenario message that contains an ID, a set of msgs, and a list of neighbors. - Adding a simple java file under utils that spits out a simple GiraphScenario.

---
+++ backend/src/main/java/stanford/infolab/debugger/utils/GiraphTestScenarioWriter.java
@@ -0,0 +1,65 @@
+package stanford.infolab.debugger.utils;
+
+import stanford.infolab.debugger.Scenario.GiraphScenario;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.giraph.utils.WritableUtils;
+import org.apache.hadoop.io.DoubleWritable;
+
+import com.google.protobuf.ByteString;
+
+/**
+ * Temporary utility class to generate test protocol buffers that store scenarios.
+ * 
+ * @author semihsalihoglu
+ */
+public class GiraphTestScenarioWriter {
+
+    public static void main(String[] args) throws IOException {
+    	String outputFileName = args[0];
+    	System.out.println("outputFileName: " + outputFileName);
+
+    	// Write a scenario that contains:
+    	// 1) vertexID: 89
+    	// 2) nbrs: 0, 1, ..., 7
+    	// 3) msgs: 0.0, 10.0, 20.0, ..., 50.0
+    	GiraphScenario.Builder giraphScenarioBuiler = GiraphScenario.newBuilder();
+    	long vertexID = 89L;
+    	giraphScenarioBuiler.setVertexId(vertexID);
+    	for (int i = 0; i <= 7; ++i) {
+    		giraphScenarioBuiler.addNbrs(i);
+    	}
+    	for (int i = 0; i <= 5; ++i) {
+    		DoubleWritable serializedDoubleWritable = new DoubleWritable(i*10.0);
+    		byte[] serializedByteArray = WritableUtils.writeToByteArray(serializedDoubleWritable);
+    		System.out.println("serializedDoubleWritableByteArray.length: "
+    			+ serializedByteArray.length);
+    		giraphScenarioBuiler.addMessages(ByteString.copyFrom(serializedByteArray));
+    	}
+    	GiraphScenario scenario = giraphScenarioBuiler.build();
+    	FileOutputStream output = new FileOutputStream(outputFileName);
+    	scenario.writeTo(output);
+    	output.close();
+
+    	// Deserialize the written pb, and verify that the deserialized scenario contains
+    	// the same contents.
+    	GiraphScenario deserializedScenario = GiraphScenario.parseFrom(
+    		new FileInputStream(outputFileName));
+    	System.out.println("deserializedScenario: " + deserializedScenario);
+    	System.out.println("deserializedScenario.vertexID: " + deserializedScenario.getVertexId());
+    	for (ByteString msgAsByteString : deserializedScenario.getMessagesList()) {
+        	DoubleWritable deserializedDoubleWritable = new DoubleWritable();
+        	byte[] byteArray = msgAsByteString.toByteArray();    		
+        	System.out.println("deserializedDoubleWritableByteArray.size: " + byteArray.length);
+        	WritableUtils.readFieldsFromByteArray(byteArray, deserializedDoubleWritable);
+        	System.out.println("Deserialized msg: " + deserializedDoubleWritable.get());
+    	}
+    	
+    	for (long nbrID : deserializedScenario.getNbrsList()) {
+    		System.out.println("nbrID: " + nbrID);
+    	}
+    }
+}
