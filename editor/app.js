00690576e9accf7402f5cca8da758426f62dcebc
Vikesh Khanna <khanna.vikesh@gmail.com>
2014-04-14 13:30:06 -0700

    Fixing JS based on PR comments

--- editor/app.js
+++ editor/app.js
@@ -24,9 +24,9 @@ function Editor(options) {
     this.setSize();
 
     this.nodes = [
-            getNewNode('0'),
-            getNewNode('1'),
-            getNewNode('2')
+            this.getNewNode('0'),
+            this.getNewNode('1'),
+            this.getNewNode('2')
         ],
     this.numNodes = 2,
     this.links = [
@@ -34,7 +34,6 @@ function Editor(options) {
             {source : this.nodes[1], target : this.nodes[2], left : false, right : true }
         ];
 
-    // Format of messages is the same as links
     // {sender: senderNodeObj, receiver: receiverNodeObj, message: message}
     this.messages  = [];
 
@@ -231,7 +230,7 @@ function getPadding(node) {
  * Returns a new node objects.
  * @param {string} id - Identifier of the node.
  */
-function getNewNode(id) {
+Editor.prototype.getNewNode = function(id) {
     return {id : id, reflexive : false, attrs : [], x: 0, y: 0};
 }
 
@@ -497,7 +496,7 @@ Editor.prototype.mousedown = function() {
 
     // Insert new node at point.
     var point = d3.mouse(d3.event.target),
-        node =  getNewNode((++this.numNodes).toString());
+        node =  this.getNewNode((++this.numNodes).toString());
     node.x = point[0];
     node.y = point[1];
     this.nodes.push(node);
@@ -513,11 +512,12 @@ Editor.prototype.getNodeIndex = function(id) {
 }
 
 /*
- * Returns the node object with the given id.
- * @param {string{ id - The identifier of the node.
+ * Returns the node object with the given id, null if node is not present.
+ * @param {string} id - The identifier of the node.
  */
 Editor.prototype.getNodeWithId = function(id) {
-    return this.nodes[this.getNodeIndex(id)];
+    var index = this.getNodeIndex(id);
+    return index >= 0 ? this.nodes[index] : null;
 }
 
 /*
@@ -525,16 +525,14 @@ Editor.prototype.getNodeWithId = function(id) {
  * Output format: {receiverId: message}
  * @param {string} id
  */
-Editor.prototype.getMessagesSent = function(id) {
+Editor.prototype.getMessagesSentByNode = function(id) {
     var messagesSent = {};
-
     for (var i = 0; i < this.messages.length; i++) {
         var messageObj = this.messages[i];
         if (messageObj.sender.id === id) {
             messagesSent[messageObj.receiver.id] = messageObj.message;
         }
     }
-
     return messagesSent;
 }
 
@@ -543,22 +541,20 @@ Editor.prototype.getMessagesSent = function(id) {
  * Output format: {senderId: message}
  * @param {string} id
  */
-Editor.prototype.getMessagesReceived = function(id) {
+Editor.prototype.getMessagesReceivedByNode = function(id) {
     var messagesReceived = {};
-
     for (var i = 0; i < this.messages.length; i++) {
         var messageObj = this.messages[i];
         if (messageObj.receiver.id === id) {
             messagesReceived[messageObj.sender.id] = messageObj.message;
         }
     }
-
     return messagesReceived;
 }
 
 /*
  * Returns true if the node with the given ID is present in the graph.
- * @param {string} id - the identifier of the node.
+ * @param {string} id - The identifier of the node.
  */
 Editor.prototype.containsNode = function(id) {
     return this.getNodeIndex(id) >= 0;
@@ -775,11 +771,12 @@ Editor.prototype.keyup = function() {
  * Format:
  * {
  *  nodeId: {
- *            adj: [adjId1, adjId2],
- *            attrs: [attr1, attr2],
+ *            adj: [adjId1, adjId2...],
+ *            attrs: [attr1, attr2...],
  *            msgs: {
  *                    receiverId1: "message1",
- *                    receiverId2: "message2"
+ *                    receiverId2: "message2",
+ *                    ...
  *                  }
  *          }
  * }
@@ -791,30 +788,29 @@ Editor.prototype.buildGraphFromAdjList = function(adjList) {
 
     // Scan every node in adj list to build the nodes array.
     for (var nodeId in adjList) {
-        if (!this.containsNode(nodeId)) {
-            var node = getNewNode(nodeId);
+        var node = this.getNodeWithId(nodeId);
+        if (node === null) {
+            node = this.getNewNode(nodeId);
             if (adjList[nodeId]['attrs']) {
                 node.attrs = adjList[nodeId]['attrs'];
             }
             this.numNodes++;
             this.nodes.push(node);
         }
-        var node = this.getNodeWithId(nodeId);
         var adj = adjList[nodeId]['adj'];
-
         // For every node in the adj list of this node,
         // add the node to this.nodes and add the edge to this.links
         for (var i = 0; i < adj.length; i++) {
             var adjId = adj[i];
-            if (!this.containsNode(adjId)) {
-                adjNode = getNewNode(adjId);
+            var adjNode = this.getNodeWithId(adjId);
+            if (adjNode === null) {
+                adjNode = this.getNewNode(adjId);
                 if (adjList[adjId] && adjList[adjId]['attrs']) {
                     adjNode.attrs = adjList[adjId]['attrs'];
                 }
                 this.numNodes++;
                 this.nodes.push(adjNode);
             }
-            var adjNode = this.getNodeWithId(adjId);
             // Add the edge.
             // Note that edges are stored as source, target where source < target.
             if (nodeId < adjId) {
@@ -823,16 +819,17 @@ Editor.prototype.buildGraphFromAdjList = function(adjList) {
                 this.links.push({source: adjNode, target: node, left: true, right: false});
             }
         }
-
         var msgs = adjList[nodeId]['msgs'];
         // Build the this.messages
         if (msgs) {
             for(var receiverId in msgs) {
-                this.messages.push({ sender: node, receiver: this.getNodeWithId(receiverId), message: msgs[receiverId]});
+                this.messages.push({ sender: node,
+                    receiver: this.getNodeWithId(receiverId),
+                    message: msgs[receiverId]
+                });
             }
         }
     }
-
     this.init();
     this.restart();
 }
