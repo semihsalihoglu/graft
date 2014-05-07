514319aec1d1e85d458c63f2ffaac7ab05e1f6cd
Vikesh Khanna <khanna.vikesh@gmail.com>
2014-05-07 02:31:47 -0700

    Fetching actual data from HDFS in the backend

--- backend/src/main/java/stanford/infolab/debugger/server/Server.java
+++ backend/src/main/java/stanford/infolab/debugger/server/Server.java
@@ -4,8 +4,22 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.io.UnsupportedEncodingException;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper.NeighborWrapper;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper.OutgoingMessageWrapper;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -21,8 +35,10 @@ public class Server {
         server.createContext("/vertices", new GetVertices());
         server.createContext("/scenario", new GetScenario());
         // Creates a default executor.
-        server.setExecutor(null); 
+        server.setExecutor(null);
+        //System.out.println(ServerUtils.readScenarioFromTrace("job_201405061024_0019", 0, "0").toString());
         server.start();
+        
     }
 
     /*
@@ -47,29 +63,41 @@ public class Server {
          * Returns superstep data of the job in JSON format.
          * TODO(vikesh): Sample/Demo method for now. Use a proper package to encode JSON.
          */
-        private String GetSuperstepData(String job_id) {
+        private String GetSuperstepData(String joId) {
                 return  "[{'PR1' : {adj: ['PR2', 'PR3'], attrs:[0.244], msgs:{ 'PR2' : 'msgFrom1To2.step1', 'PR3' : 'msgFrom1To3.step-1'}}, 'PR2' : {adj: ['PR3'], attrs:    [0.455], msgs: {'PR3': 'msgTo3From2.step-1'}}, 'PR4' : {adj: ['PR1'], attrs:[0.78]}},  {'PR1' : {attrs:[0.448], msgs:{ 'PR2' : 'msgFrom1To2.step0', 'PR3' : 'msgFrom    1To3.step0'}}, 'PR2' : {attrs:[0.889], msgs: {'PR3': 'msgTo3From2.step0'}}, 'PR4' : {attrs:[0.98]}}, {'PR1' : {attrs:[0.001], msgs:{ 'PR2' : 'msgFrom1To2.step1', 'P    R3' : 'msgFrom1To3.step1'}}, 'PR2' : {attrs:[0.667], msgs: {'PR3': 'msgTo3From2.step1'}}}, {'PR1' : {attrs:[0.232], msgs:{ 'PR2' : 'msgFrom1To2.step2', 'PR3' : 'msg    From1To3.step2'}}, 'PR2' : {attrs:[0.787], msgs: {'PR3': 'msgTo3From2.step2'}}}]";
         }
     }
 
     /*
-     * Returns the list of vertices debugged in a given superstep for a given job. 
+     * Returns the list of vertices debugged in a given Superstep for a given job. 
      * @URLParams: {jobId, superstepId}
      */
     static class GetVertices extends ServerHttpHandler {
         public void processRequest(HashMap<String, String> paramMap) {
             String jobId = paramMap.get(ServerUtils.JOB_ID_KEY);
             String superstepId = paramMap.get(ServerUtils.SUPERSTEP_ID_KEY);
-
-            // TODO(vikesh): Replace with actual data.
-            if (jobId != null && superstepId != null) {
-                this.statusCode = HttpURLConnection.HTTP_OK;
-                this.response = "['vertexId1', 'vertexId2', 'vertexId3', 'vertexId4']";
-            } else {
-                this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
-                this.response = String.format("Invalid parameters. %s and %s are mandatory parameter.",
-                        ServerUtils.JOB_ID_KEY, ServerUtils.SUPERSTEP_ID_KEY);
+            
+            // jobId and superstepId are mandatory. Validate.
+            if (jobId == null || superstepId == null ) {
+            	 this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
+                 this.response = String.format("Invalid parameters. %s and %s are mandatory parameter.",
+                         ServerUtils.JOB_ID_KEY, ServerUtils.SUPERSTEP_ID_KEY);
+                 return;
             }
+            
+        	ArrayList<String> vertexIds = null;
+        	try {
+        		vertexIds = ServerUtils.getVerticesDebugged("job_201405061024_0019", 0);
+        	}
+        	catch(IOException ex) {
+        		// IOException is unexpected in this case. Return Internal Server Error.
+        		this.statusCode = HttpURLConnection.HTTP_INTERNAL_ERROR;
+        		this.response = "Internal Server Error.";
+        		return;
+        	}
+            this.statusCode = HttpURLConnection.HTTP_OK;
+            // Returns output as an array ["id1", "id2", "id3" .... ]
+            this.response = new JSONArray(vertexIds).toString();
         }
     }
 
@@ -84,28 +112,89 @@ public class Server {
         public void processRequest(HashMap<String, String> paramMap) {
             String jobId = paramMap.get(ServerUtils.JOB_ID_KEY);
             String superstepId = paramMap.get(ServerUtils.SUPERSTEP_ID_KEY);
-
-            // TODO(vikesh): Replace with actual data and handle multiple/single vertices properly.
-            if (jobId != null && superstepId != null) {
-                this.statusCode = HttpURLConnection.HTTP_OK;
-                // Get the single vertexId or the list of vertexIds (comma-separated).
-                String rawVertexIds = paramMap.get(ServerUtils.VERTEX_ID_KEY);
-                // No vertex Id supplied. Return scenario for all vertices.
-                if (rawVertexIds == null) {
-                    //TODO(vikesh) : Read scenario for all vertices.
-                }
-                else {
-                    String[] vertexIds = rawVertexIds.split(",");
-                    for(String vertexId : vertexIds) {
-                        // TODO(vikesh) : Read scenario for each vertex in the input.
-                    }
-                }
-                this.response = "{'vertexId1' : {}, 'vertexId2' : {}}";
-            } else {
-                this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
-                this.response = String.format("Invalid parameters. %s and %s are mandatory parameter.", 
-                    ServerUtils.JOB_ID_KEY, ServerUtils.SUPERSTEP_ID_KEY);
+            
+            // Check both jobId and superstepId are present
+            if (jobId == null || superstepId == null) {
+            	this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
+            	this.response = String.format("Invalid parameters. %s and %s are mandatory parameter.", 
+                        ServerUtils.JOB_ID_KEY, ServerUtils.SUPERSTEP_ID_KEY);
+                return;
             }
-        }
+            Long superstepNo;
+            // Parse integral value of superstep from parameter. 
+            try { 
+            	superstepNo = Long.parseLong(paramMap.get(ServerUtils.SUPERSTEP_ID_KEY));
+            	
+            	if (superstepNo < -1) {
+            		throw new NumberFormatException("Superstep must be >= -1");
+            	}
+            }
+            catch (NumberFormatException ex) {
+                this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
+                this.response = String.format("%s must be an integer >= -1.", 
+            			ServerUtils.SUPERSTEP_ID_KEY);
+                return;
+            }
+            
+            JSONObject scenarioObj = new JSONObject();
+            ArrayList<String> vertexIds = null;
+           	// Get the single vertexId or the list of vertexIds (comma-separated).
+            String rawVertexIds = paramMap.get(ServerUtils.VERTEX_ID_KEY);
+      
+            // No vertex Id supplied. Return scenario for all vertices.
+            if (rawVertexIds == null) {
+                // Read scenario for all vertices. 
+            	try {
+            		vertexIds = ServerUtils.getVerticesDebugged(jobId, superstepNo);
+            	}
+            	catch(IOException ex) {
+            		this.statusCode = HttpURLConnection.HTTP_INTERNAL_ERROR;
+            		this.response = "Internal Server Error";
+            		return;
+            	}
+            }
+            else {
+            	// Split the vertices by comma.
+                vertexIds = new ArrayList(Arrays.asList(rawVertexIds.split(",")));
+            }
+            
+            // For each vertex, read the scenario and add to the JSON object.
+            for(String vertexId : vertexIds) {
+            	GiraphScenarioWrapper giraphScenarioWrapper;
+                
+            	try {
+            		giraphScenarioWrapper =
+            				ServerUtils.readScenarioFromTrace(jobId, superstepNo, vertexId.trim());
+            	}
+            	catch(IOException ex) {
+            		// IOException could be caused by an error on the server side
+            		// or if the vertex was not debugged. 
+            		this.statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
+            		this.response = "Could not read the debug trace for this vertex. " +
+            				String.format("Please ensure vertex %d was debugged in superstep %s",
+            						vertexId, superstepId);
+            		return;
+            	}
+            	catch(ClassNotFoundException ex) {
+            		this.statusCode = HttpURLConnection.HTTP_INTERNAL_ERROR;
+            		this.response = "Internal Server error.";
+            		return;
+            	}
+            	
+            	// Add this node's scenario to the final JSON
+            	try {
+            		scenarioObj.put(vertexId, ServerUtils.scenarioToJSON(giraphScenarioWrapper));
+            	}
+            	catch(JSONException ex) {
+            		this.statusCode = HttpURLConnection.HTTP_INTERNAL_ERROR;
+            		this.response = "Server error.";
+            		return;
+            	}
+            }
+            
+            // Set status as OK and convert JSONObject to string.
+            this.statusCode = HttpURLConnection.HTTP_OK;
+            this.response = scenarioObj.toString();
+        } 
     }
 }
--- backend/src/main/java/stanford/infolab/debugger/server/ServerUtils.java
+++ backend/src/main/java/stanford/infolab/debugger/server/ServerUtils.java
@@ -1,9 +1,29 @@
 package stanford.infolab.debugger.server;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 import java.net.URLDecoder;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.io.File;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper.NeighborWrapper;
+import stanford.infolab.debugger.utils.GiraphScenearioSaverLoader;
+import stanford.infolab.debugger.utils.GiraphScenarioWrapper.ContextWrapper.OutgoingMessageWrapper;
+
 import com.sun.net.httpserver.Headers;
+import java.util.regex.Matcher;
 
 /*
  * Utility methods for Debugger Server.
@@ -36,5 +56,90 @@ public class ServerUtils {
     public static void setMandatoryResponseHeaders(Headers headers) {
         // TODO(vikesh): **REMOVE CORS FOR ALL AFTER DECIDING THE DEPLOYMENT ENVIRONMENT**
         headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Content-Type", "application/json");
+    }
+    
+    /*
+     * Returns the HDFS FileSystem reference. 
+     */
+    public static FileSystem getFileSystem() throws IOException {
+    	String coreSitePath = "/usr/local/hadoop/conf/core-site.xml";
+        Configuration configuration = new Configuration();
+        configuration.addResource(new Path(coreSitePath));
+        FileSystem fs = FileSystem.get(configuration);
+        return fs;
+    }
+    /*
+     * Reads the protocol buffer trace corresponding to the given jobId, superstepNo and vertexId
+     * and returns the giraphScenarioWrapper. 
+     * @param jobId : ID of the job debugged.
+     * @param superstepNo: Superstep number debugged.
+     * @param vertexId - ID of the vertex debugged. 
+     * Returns GiraphScenarioWrapper. 
+     */
+    public static GiraphScenarioWrapper readScenarioFromTrace(String jobId, 
+    		long superstepNo, String vertexId) throws IOException, ClassNotFoundException  {
+    	FileSystem fs = ServerUtils.getFileSystem();
+        String traceFilePath = String.format("/%s/tr_stp_%d_vid_%s.tr", jobId, superstepNo, vertexId);
+        GiraphScenearioSaverLoader giraphSaverLoader = new GiraphScenearioSaverLoader<>();
+        GiraphScenarioWrapper giraphScenarioWrapper = 
+        		giraphSaverLoader.loadFromHDFS(fs, traceFilePath);
+        return giraphScenarioWrapper;
+    }
+    
+    /*
+     * Converts a Giraph Scenario (giraphScenarioWrapper object) to JSON (JSONObject)
+     * @param giraphScenarioWrapper : Giraph Scenario object.
+     */
+    public static JSONObject scenarioToJSON(GiraphScenarioWrapper giraphScenarioWrapper) 
+    		throws JSONException {
+    	ContextWrapper contextWrapper = giraphScenarioWrapper.getContextWrapper();
+    	JSONObject scenarioObj = new JSONObject();
+        scenarioObj.put("vertexId", contextWrapper.getVertexIdWrapper());
+        scenarioObj.put("vertexValue", contextWrapper.getVertexValueAfterWrapper());
+        JSONObject outgoingMessagesObj = new JSONObject();
+        ArrayList<String> neighborsList = new ArrayList<String>();
+        
+        for(Object outgoingMessage : contextWrapper.getOutgoingMessageWrappers()) {
+        	OutgoingMessageWrapper outgoingMessageWrapper = (OutgoingMessageWrapper) outgoingMessage;
+        	outgoingMessagesObj.put(outgoingMessageWrapper.destinationId.toString(), 
+        			outgoingMessageWrapper.message.toString());
+        }
+        
+        for(Object neighbor : contextWrapper.getNeighborWrappers()) {
+        	NeighborWrapper neighborWrapper = (NeighborWrapper) neighbor;
+        	neighborsList.add(neighborWrapper.getNbrId().toString());
+        }
+        
+        scenarioObj.put("outgoingMessages", outgoingMessagesObj);
+        scenarioObj.put("neighbors", neighborsList);
+        
+        return scenarioObj;
+    }
+    
+    /*
+     * Returns a list of vertice Ids that were debugged in the given superstep
+     * by reading (the file names of) the debug traces on HDFS. File names
+     * follow the tr_stp_<superstepNo>_vid_<vertexId>.tr naming convention.
+     */
+    public static ArrayList<String> getVerticesDebugged(String jobId, long superstepNo) 
+    		throws IOException {
+    	ArrayList<String> vertexIds = new ArrayList<String>();
+    	FileSystem fs = ServerUtils.getFileSystem();
+        String traceDirectory = String.format("/%s", jobId);
+        // Use this regex to match the file name and capture the vertex id.
+        String regex = String.format("tr_stp_%d_vid_(.*?).tr$", superstepNo);
+        Pattern p = Pattern.compile(regex);
+        Path pt = new Path(traceDirectory);
+        // Iterate through each file in this directory and match the regex.
+        for (FileStatus fileStatus: fs.listStatus(pt)) {
+        	String fileName = new File(fileStatus.getPath().toString()).toString();
+        	Matcher m = p.matcher(fileName);
+        	// Add this vertex id if there is a match.
+        	if (m.find()) {
+        		vertexIds.add(m.group(1));
+        	}
+        }
+    	return vertexIds;
     }
 }
