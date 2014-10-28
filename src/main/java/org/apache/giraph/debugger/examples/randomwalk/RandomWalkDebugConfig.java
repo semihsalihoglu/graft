1e4c06607e19f5acfb5ceaf073b373a6369c31a8
Jaeho Shin <netj@sparcs.org>
2014-10-28 16:11:45 -0700

    DebugConfigs for RandomWalkComputation

---
+++ src/main/java/org/apache/giraph/debugger/examples/randomwalk/RandomWalkDebugConfig.java
@@ -0,0 +1,33 @@
+package org.apache.giraph.debugger.examples.randomwalk;
+
+import org.apache.giraph.debugger.DebugConfig;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+
+public class RandomWalkDebugConfig
+  extends
+  DebugConfig<LongWritable, IntWritable, NullWritable, IntWritable, IntWritable> {
+
+  @Override
+  public boolean shouldCheckVertexValueIntegrity() {
+    return true;
+  }
+
+  @Override
+  public boolean isVertexValueCorrect(LongWritable vertexId, IntWritable value) {
+    return value.get() > 0;
+  }
+
+  @Override
+  public boolean shouldCheckMessageIntegrity() {
+    return true;
+  }
+
+  @Override
+  public boolean isMessageCorrect(LongWritable srcId, LongWritable dstId,
+    IntWritable message) {
+    return message.get() > 0;
+  }
+
+}
